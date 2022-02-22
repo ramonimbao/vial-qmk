ab654f5491dbe125466d3de21f32f4453cae33b2
Ramon Imbao <ramonimbao@gmail.com>
2022-02-22 21:14:40 +0800

    Add GHS.RAR VIAL support

---
+++ keyboards/ghs/rar/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE7, 0x2F, 0x70, 0x66, 0xE0, 0xB6, 0x28, 0x87}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 10 }
+#define VIAL_UNLOCK_COMBO_COLS { 0,  3 }
