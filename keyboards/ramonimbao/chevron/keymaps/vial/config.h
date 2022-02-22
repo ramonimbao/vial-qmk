e4e193a308fd04cc077186f2a15bbd975cb4546c
Ramon Imbao <ramonimbao@gmail.com>
2022-02-22 22:41:23 +0800

    Add Chevron VIAL support

---
+++ keyboards/ramonimbao/chevron/keymaps/vial/config.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x6A, 0x10, 0x83, 0x15, 0x6B, 0xD2, 0x87, 0x6C}
+
+#define VIAL_ENCODER_DEFAULT { \
+KC_VOLD, KC_VOLU, \
+_______, _______, \
+_______, _______, \
+_______, _______ }
