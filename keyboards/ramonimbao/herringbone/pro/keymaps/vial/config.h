b30a5d623446c8465def23274212f157412cd4e1
Ramon Imbao <ramonimbao@gmail.com>
2022-02-23 12:56:11 +0800

    Add Herringbone Pro VIAL support

---
+++ keyboards/ramonimbao/herringbone/pro/keymaps/vial/config.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xCB, 0x1A, 0x1A, 0xCA, 0xE0, 0xCF, 0x1E, 0x49}
+
+#define VIAL_ENCODER_DEFAULT {\
+KC_VOLD, KC_VOLU, \
+_______, _______, \
+_______, _______, \
+_______, _______, \
+}
