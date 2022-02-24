b4516662158f208826465ea58210c0e08646ad37
Ramon Imbao <ramonimbao@gmail.com>
2022-02-24 19:18:01 +0800

    Add SquishyFRL VIA support

---
+++ keyboards/ramonimbao/squishyfrl/keymaps/vial/config.h
@@ -0,0 +1,20 @@
+/*
+Copyright 2021 Ramon Imbao
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x15, 0x76, 0xD0, 0x69, 0x7C, 0x7F, 0x58, 0x13}
