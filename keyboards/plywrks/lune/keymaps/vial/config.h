063e0e1b7aeb02445b05cd00145519b23584b60e
Ramon Imbao <ramonimbao@gmail.com>
2022-05-11 17:37:30 +0800

    Add Plywrks Lune VIAL support

---
+++ keyboards/plywrks/lune/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3A, 0x8F, 0x74, 0x77, 0xAE, 0x75, 0xDC, 0x6C}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 5 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 7 }
