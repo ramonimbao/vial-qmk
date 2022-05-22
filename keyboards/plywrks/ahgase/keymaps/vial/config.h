f72abbb06e3f46770fc61a52b98e71f3ea4938f6
Ramon Imbao <ramonimbao@gmail.com>
2022-05-23 07:52:08 +0800

    Add Plywrks Ahgase VIAL support

---
+++ keyboards/plywrks/ahgase/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xBA, 0x9B, 0x40, 0x54, 0x4C, 0x44, 0x6D, 0x3E}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 5 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 7 }
