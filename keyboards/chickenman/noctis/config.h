e66c364c55a813e7200743a163fb659ed6dd4f7a
Ramon Imbao <ramonimbao@gmail.com>
2022-05-28 08:59:37 +0800

    Fix incorrect matrix

--- keyboards/chickenman/noctis/config.h
+++ keyboards/chickenman/noctis/config.h
@@ -13,8 +13,10 @@
 #define MATRIX_ROWS 6
 #define MATRIX_COLS 20
 
+//                         0   1   2   3   4   5
 #define MATRIX_ROW_PINS { 29, 19, 23, 24, 25, 26 }
-#define MATRIX_COL_PINS { 28, 27, 18, 17, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }
+//                         0   1   2   3   4   5   6   7   8   9 10 11 12 13 14 15 16 17 18 19
+#define MATRIX_COL_PINS { 28, 27, 18, 17, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 3, 4, 2, 1, 0 }
 
 #define DIODE_DIRECTION COL2ROW
 
